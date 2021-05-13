2284c7a7f477537d2485f7b5d8a7a047c5719dfc
Trung <tqtrung1209@gmail.com>
2021-05-13 14:29:35 +0700

    add fav icon?

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,15 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+require("semantic-ui-sass")
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+
+$(document).on('turbolinks:load', function(){
+  $('.ui.dropdown').dropdown();
+
+})
+
